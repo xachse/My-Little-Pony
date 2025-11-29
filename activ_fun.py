4025cd9eb13cb2d131f29fb4a222accf92b9929c
Z_BM <58821444+Nayjer@users.noreply.github.com>
2025-11-29 21:22:33 +0100

    Update activ_fun.py

--- activ_fun.py
+++ activ_fun.py
@@ -4,7 +4,7 @@ import sklearn
 
 class Network():
 
-    def __init__(self, sizes=[64, 64, 32, 3]):
+    def __init__(self, dataset='mnist'):
         '''
         Diese Funktion initilaiisert zufällig, gleichverteilte Gewichte und Biases für das Netzwerk
         mit gegebenen Anzahlen der Neuronen pro Schicht. In unserem Fall sind dies 64/28^2 Input-Neuronen,
@@ -12,26 +12,40 @@ class Network():
         Anschließend werden die Test- und Trainingsdaten importiert und vorbereitet.
         '''
         self.num_layers = 3
-        self.sizes = sizes
         self.biases = []
-        for i in range(3):
-            self.biases.append(np.random.randn(sizes[i + 1], 1))
         self.weights = []
-        for i in range(3):
-            self.weights.append(np.random.randn(sizes[i + 1], sizes[i])) 
 
-        # Nun werden die Test- und Trainingsdaten importiert
+        if dataset == "digits":
+            self.sizes = [64, 64, 32, 3]
+            for i in range(3):
+                self.biases.append(np.random.randn(self.sizes[i + 1], 1))
+            for i in range(3):
+                self.weights.append(np.random.randn(self.sizes[i + 1], self.sizes[i])) 
+            self.load_digits()
+        elif dataset == "mnist":
+            self.sizes = [784, 64, 32, 3]
+            self.biases = [np.zeros((y, 1)) for y in self.sizes[1:]]
+            self.weights = [np.random.randn(y, x) * np.sqrt(1 / x) for x, y in zip(self.sizes[:-1], self.sizes[1:])]
+            self.load_mnist()
+        else:
+            raise ValueError("Unbekannter Datensatz")
 
+
+    def load_digits(self):
         digits = sklearn.datasets.load_digits()
         X = digits.data  # Matrix mit 1797 Zeilen (Anzahl der Bilder, nach Dokumentation) und 64 Spalten (8x8 Bilder)    
         y = digits.target  # Labels (Zahlen von 0 bis 9)
+		
         mask = np.isin(y, [1, 5, 7])
         X = X[mask]
         y = y[mask]
+		
         label_map = {1: 0, 5: 1, 7: 2}
         y = np.array([label_map[label] for label in y])  # Labels werden umgewandelt zu 0, 1, 2
+		
         X_train, X_test, y_train, y_test = sklearn.model_selection.train_test_split(X, y, test_size=0.2, random_state=42, stratify=y)  # Split 
         scaler = sklearn.preprocessing.StandardScaler()  # Skalierung 
+		
         X_train = scaler.fit_transform(X_train)
         X_test  = scaler.transform(X_test)
         X_train = [x.reshape(64, 1) for x in X_train] # zu Spaltenvektoren umformen
@@ -42,6 +56,31 @@ class Network():
 
         self.training_data = training_data
         self.test_data = test_data
+
+    
+    def load_mnist(self):
+        mnist = sklearn.datasets.fetch_openml("mnist_784", version=1, as_frame=False)
+        X = mnist["data"]
+        y = mnist["target"].astype(int)
+
+        mask = np.isin(y, [1,5,7])
+        X = X[mask]
+        y = y[mask]
+
+        label_map = {1:0, 5:1, 7:2}
+        y = np.array([label_map[k] for k in y])
+
+        X_train, X_test, y_train, y_test = sklearn.model_selection.train_test_split(X, y, test_size=0.2, random_state=42, stratify=y)
+
+        scaler = sklearn.preprocessing.StandardScaler()
+        X_train = scaler.fit_transform(X_train)
+        X_test = scaler.transform(X_test)
+
+        X_train = [x.reshape(784,1) for x in X_train]
+        X_test = [x.reshape(784,1) for x in X_test]
+
+        self.training_data = list(zip(X_train, y_train))
+        self.test_data = list(zip(X_test, y_test))
 
 
     def one_hot_encode(self, j):
@@ -86,6 +125,43 @@ class Network():
             test_results.append((pred, y))
         return sum(int(x == y) for (x, y) in test_results)
     
+
+    # Stochastic Gradient Descent (SGD) Methode von Michael Nielsen's Buch "Neural Networks and Deep Learning"
+
+    def SGD(self, training_data, epochs, mini_batch_size, eta):
+        """Train the neural network using mini-batch stochastic
+        gradient descent.  The ``training_data`` is a list of tuples
+        ``(x, y)`` representing the training inputs and the desired
+        outputs.  The other non-optional parameters are
+        self-explanatory.  If ``test_data`` is provided then the
+        network will be evaluated against the test data after each
+        epoch, and partial progress printed out.  This is useful for
+        tracking progress, but slows things down substantially."""
+        n = len(training_data)
+        for j in range(epochs):
+            mini_batches = [training_data[k:k+mini_batch_size] for k in range(0, n, mini_batch_size)]
+            for mini_batch in mini_batches:
+                self.update_mini_batch(mini_batch, eta)
+
+
+    def update_mini_batch(self, mini_batch, eta):
+        """Update the network's weights and biases by applying
+        gradient descent using backpropagation to a single mini batch.
+        The ``mini_batch`` is a list of tuples ``(x, y)``, and ``eta``
+        is the learning rate."""
+        nabla_b = [np.zeros(b.shape) for b in self.biases]
+        nabla_w = [np.zeros(w.shape) for w in self.weights]
+        for x, y in mini_batch:
+            delta_nabla_w, delta_nabla_b = self.backprop(x, y)
+            nabla_b = [nb+dnb for nb, dnb in zip(nabla_b, delta_nabla_b)]
+            nabla_w = [nw+dnw for nw, dnw in zip(nabla_w, delta_nabla_w)]
+        self.weights = [w-(eta/len(mini_batch))*nw
+                        for w, nw in zip(self.weights, nabla_w)]
+        self.biases = [b-(eta/len(mini_batch))*nb
+                       for b, nb in zip(self.biases, nabla_b)]
+    
+    # Stochastic Gradient Descent (SGD) Methode von Michael Nielsen's Buch "Neural Networks and Deep Learning"
+
 
     def backprop(self, x, y_true):
         """
@@ -164,53 +240,18 @@ class Network():
 
     def softplus_prime(self, x):
         return self.sigmoid(x)
+    
 
-
-def softplus(x):
-	return np.log(1+np.exp(x))
-
-# d/dx softplus(x)
-def softplus_prime(x):
-	#Funfact: Softplus_prime=sigmoid
-	
-	ex=np.exp(x)
-	
-	return ex/(1+ex)
-
-def sigmoid(x):
-	return 1/(1+np.exp(-x))
-
-# d/dx sigmoid(x)
-def sigmoid_prime(x):
-	ex=np.exp(-x)
-	
-	return ex/((1+ex)**2)
 
 if __name__ == "__main__":
-	net = Network()
-	net.train_full_batch(net.training_data, epochs=100, lr=0.2)
+    net = Network()
+    
+    '''net.train_full_batch(net.training_data, epochs=100, lr=0.2)
     accuracy = net.evaluate(net.test_data) 
-    print(f"Genauigkeit nach Training: {accuracy} von {len(net.test_data)} Testbeispielen korrekt klassifiziert.")
-	
-	# Definitionsbereich
-	x = np.linspace(-5, 5, 400)
-	
-	# Funktion
-	y = sigmoid(x)
-	plt.plot(x, y, label="f(x) = sigmoid")
-	
-	y=sigmoid_prime(x)
-	plt.plot(x, y, label="f'(x) = d/dx sigmoid")
-	
-	y=softplus(x)
-	plt.plot(x, y, label="g(x) = softplus")
-	
-	y=softplus_prime(x)
-	plt.plot(x, y, label="g'(x) = d/dx softplus")
-	
-	plt.xlabel("x")
-	plt.ylabel("f(x)")
-	plt.title("")
-	plt.legend()
-	
-	plt.show()
+    print(f"Genauigkeit nach Training: {accuracy} von {len(net.test_data)} Testbeispielen korrekt klassifiziert.")'''
+
+    net2 = Network() 
+    net2.SGD(net2.training_data, epochs=30, mini_batch_size=500, eta=0.1) 
+    accuracy2 = net2.evaluate(net2.test_data) 
+    print(f"Genauigkeit nach SGD: {accuracy2} von {len(net2.test_data)} Testbeispielen korrekt klassifiziert.")
+
